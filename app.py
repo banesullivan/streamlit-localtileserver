ac397eb150c95cee0d0915ac3d1332823abb250c
banesullivan <bane.sullivan@kitware.com>
2022-04-29 11:56:55 -0600

    Cleanup from debugging

--- app.py
+++ app.py
@@ -5,18 +5,13 @@ import folium
 import streamlit as st
 from appdirs import user_data_dir
 from localtileserver import TileClient, get_folium_tile_layer
-from localtileserver.validate import (
-    ValidateCloudOptimizedGeoTIFFException,
-    validate_cog,
-)
+from localtileserver.validate import (ValidateCloudOptimizedGeoTIFFException,
+                                      validate_cog)
 from streamlit_folium import folium_static
 
 "# streamlit-localtileserver"
 
-if os.environ.get("JUPYTERHUB_SERVICE_PREFIX") is not None:
-    os.environ[
-        "LOCALTILESERVER_CLIENT_PREFIX"
-    ] = f"{os.environ['JUPYTERHUB_SERVICE_PREFIX'].lstrip('/')}/proxy/{{port}}"
+# TODO: set `"LOCALTILESERVER_CLIENT_PREFIX"`
 
 
 def upload_file_to_path(uploaded_file):
@@ -26,11 +21,14 @@ def upload_file_to_path(uploaded_file):
         f.write(uploaded_file.getvalue())
     return str(path.absolute())
 
+
 st.write(os.environ)
 
 
 uploaded_file = st.file_uploader("Upload a raster")
-url = st.text_input("Or input a URL (try https://data.kitware.com/api/v1/file/626854a14acac99f42126a74/download)")
+url = st.text_input(
+    "Or input a URL (try https://data.kitware.com/api/v1/file/626854a14acac99f42126a74/download)"
+)
 if uploaded_file or url:
     if uploaded_file is not None:
         client = TileClient(upload_file_to_path(uploaded_file))
