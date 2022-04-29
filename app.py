b414f8e89d7929fdd5ca2450610885dcad452a34
banesullivan <bane.sullivan@kitware.com>
2022-04-29 13:01:13 -0600

    Add metadata sidebar

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import argparse
-from pathlib import Path
 import sys
+from pathlib import Path
 
 import folium
 import streamlit as st
 from appdirs import user_data_dir
 from folium.plugins import Fullscreen
 from localtileserver import TileClient, get_folium_tile_layer
-from localtileserver.validate import (
-    ValidateCloudOptimizedGeoTIFFException,
-    validate_cog,
-)
+from localtileserver.validate import (ValidateCloudOptimizedGeoTIFFException,
+                                      validate_cog)
 from streamlit_folium import folium_static
+
+st.set_page_config(page_title="streamlit-localtileserver")
 
 "# streamlit-localtileserver"
 
@@ -19,8 +19,8 @@ from streamlit_folium import folium_static
 
 
 def parse_args(args):
-    parser = argparse.ArgumentParser('Data Diagnostics')
-    parser.add_argument('-f', '--filename', help='Local path or URL', required=False)
+    parser = argparse.ArgumentParser("Data Diagnostics")
+    parser.add_argument("-f", "--filename", help="Local path or URL", required=False)
     return parser.parse_args(args)
 
 
@@ -31,10 +31,12 @@ def upload_file_to_path(uploaded_file):
         f.write(uploaded_file.getvalue())
     return str(path.absolute())
 
+
 args = parse_args(sys.argv[1:])
 uploaded_file = st.file_uploader("Upload a raster")
 url = st.text_input(
-    "Or input a URL (try https://data.kitware.com/api/v1/file/626854a14acac99f42126a74/download)"
+    "Or input a URL, try: https://data.kitware.com/api/v1/file/626854a14acac99f42126a74/download",
+    placeholder="https://data.kitware.com/api/v1/file/626854a14acac99f42126a74/download",
 )
 arg_path = args.filename
 
@@ -55,7 +57,14 @@ if uploaded_file or url or arg_path:
 
     m = folium.Map(location=client.center(), zoom_start=client.default_zoom)
     m.add_child(layer)
+
+    with st.sidebar:
+        st.image(str(client.thumbnail()))
+        st.write("Metadata")
+        st.json(client.metadata())
 else:
+    with st.sidebar:
+        pass
     m = folium.Map()
 
 Fullscreen().add_to(m)
